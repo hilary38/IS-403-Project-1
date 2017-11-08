f0f0a0c52c89aed361855c5104d926470417d8b1
hilary38 <32460688+hilary38@users.noreply.github.com>
2017-11-08 15:46:28 -0700

    Adding Project Solution

---
+++ Project 1 IS 403/Project 1 IS 403/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Project_1_IS_403
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Project 1 IS 403/Project 1 IS 403/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Project_1_IS_403.MvcApplication" Language="C#" %>
