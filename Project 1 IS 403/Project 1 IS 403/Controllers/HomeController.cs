1e3e4c1c16fa5905cc179e92d14dcf02248dcd3e
mizozobu <hamc.m.so.j12@gmail.com>
2017-11-09 10:35:15 -0700

    dropdown

--- Project 1 IS 403/Project 1 IS 403/Controllers/HomeController.cs
+++ Project 1 IS 403/Project 1 IS 403/Controllers/HomeController.cs
@@ -23,7 +23,12 @@ namespace Project_1_IS_403.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
+            List<SelectListItem> states = new List<SelectListItem>();            
+            states.Add(new SelectListItem { Text = "Question", Value = "0", Selected = true });
+            states.Add(new SelectListItem { Text = "Request a page for a mission", Value = "1" });
+            states.Add(new SelectListItem { Text = "Suggestion", Value = "2" });
+            states.Add(new SelectListItem { Text = "Report a issue", Value = "3" });
+            ViewBag.StateNames = states;            
             return View();
         }
     }
