75fccd6d1f5fbb7d1deba91a63bf1682afc141ff
hilary38 <32460688+hilary38@users.noreply.github.com>
2017-11-09 13:59:33 -0700

    Dropdown Parameter

--- Project 1 IS 403/Project 1 IS 403/Controllers/MissionController.cs
+++ Project 1 IS 403/Project 1 IS 403/Controllers/MissionController.cs
@@ -11,18 +11,27 @@ namespace Project_1_IS_403.Controllers
         // GET: Mission
         public ActionResult Missions()
         {
-            //List<SelectListItem> mission = new List<SelectListItem>();
-            //mission.Add(new SelectListItem { Text = "Florida Jacksonville Mission", Value = "0", Selected = true });
-            //mission.Add(new SelectListItem { Text = "San Antonio Texas Mission", Value = "1" });
-            //mission.Add(new SelectListItem { Text = "California Oakland/San Francisco Mission", Value = "2" });
-            //ViewBag.contentType = mission;
+            List<SelectListItem> mission = new List<SelectListItem>();
+            mission.Add(new SelectListItem { Text = "Florida Jacksonville Mission", Value = "0", Selected = true });
+            mission.Add(new SelectListItem { Text = "San Antonio Texas Mission", Value = "1" });
+            mission.Add(new SelectListItem { Text = "California Oakland/San Francisco Mission", Value = "2" });
+            ViewBag.contentType = mission;
             return View();
         }
-        public ActionResult MissionFAQ(string sMission)
+        public ActionResult MissionFAQ(string contentType)
         {
-            string sMiss = sMission;
-
-            ViewBag.Mission = sMiss;
+            if (contentType.Equals("0"))
+            {
+                ViewBag.Mission = "Florida Jacksonville Mission";
+            }
+            else if (contentType.Equals("1"))
+            {
+                ViewBag.Mission = "San Antonio Texas Mission";
+            }
+            else if (contentType.Equals("2"))
+            {
+                ViewBag.Mission = "California Oakland/San Francisco Mission";
+            }
 
             return View();
         }
