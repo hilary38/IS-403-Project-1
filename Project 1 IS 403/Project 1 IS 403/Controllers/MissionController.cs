62b51b6ff56322b134c00f7608ad4dd2ac0696bc
riahsheffield <32337876+riahsheffield@users.noreply.github.com>
2017-11-09 13:22:01 -0700

    pass parameters

--- Project 1 IS 403/Project 1 IS 403/Controllers/MissionController.cs
+++ Project 1 IS 403/Project 1 IS 403/Controllers/MissionController.cs
@@ -11,11 +11,11 @@ namespace Project_1_IS_403.Controllers
         // GET: Mission
         public ActionResult Missions()
         {
-            List<SelectListItem> mission = new List<SelectListItem>();
-            mission.Add(new SelectListItem { Text = "Florida Jacksonville Mission", Value = "0", Selected = true });
-            mission.Add(new SelectListItem { Text = "San Antonio Texas Mission", Value = "1" });
-            mission.Add(new SelectListItem { Text = "California Oakland/San Francisco Mission", Value = "2" });
-            ViewBag.contentType = mission;
+            //List<SelectListItem> mission = new List<SelectListItem>();
+            //mission.Add(new SelectListItem { Text = "Florida Jacksonville Mission", Value = "0", Selected = true });
+            //mission.Add(new SelectListItem { Text = "San Antonio Texas Mission", Value = "1" });
+            //mission.Add(new SelectListItem { Text = "California Oakland/San Francisco Mission", Value = "2" });
+            //ViewBag.contentType = mission;
             return View();
         }
         public ActionResult MissionFAQ(string sMission)
