5bc3d6458b2bad66c26c866fda5226033a972369
hilary38 <32460688+hilary38@users.noreply.github.com>
2017-12-11 15:28:39 -0700

    adding users model

--- Project 1 IS 403/Project 1 IS 403/Models/User.cs
+++ Project 1 IS 403/Project 1 IS 403/Models/User.cs
@@ -2,19 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Project_1_IS_403.Models
 {
     public class User
     {
+        [Key]
+        [DisplayName("User ID")]
+        [Required(ErrorMessage = "A user ID is required")]
         public int userID { get; set; }
 
+        [DisplayName("User email")]
+        [Required(ErrorMessage = "A user email is required")]
         public string userEmail { get; set; }
 
+        [DisplayName("User Password")]
+        [Required(ErrorMessage = "A user password is required")]
         public string userPassword { get; set; }
 
+        [DisplayName("First Name")]
+        [Required(ErrorMessage = "A first name is required")]
         public string firstName { get; set; }
 
+        [DisplayName("Last Name")]
+        [Required(ErrorMessage = "A last name is required")]
         public string lastName { get; set; }
 
     }
