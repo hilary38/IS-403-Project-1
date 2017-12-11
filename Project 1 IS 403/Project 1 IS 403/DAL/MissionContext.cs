58907dbbac644c5c6f9bd20b7066aec3b8151b0a
hilary38 <32460688+hilary38@users.noreply.github.com>
2017-12-11 16:26:59 -0700

    heyyy

--- Project 1 IS 403/Project 1 IS 403/DAL/MissionContext.cs
+++ Project 1 IS 403/Project 1 IS 403/DAL/MissionContext.cs
@@ -2,22 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using Project_1_IS_403.Models;
-using Sytem.Data.Entity;
-
 
 namespace Project_1_IS_403.DAL
 {
-    public class MissionContext : DbContext
+    public class MissionContext
     {
-        public MissionContext() : base("MissionContext")
-        {
-
-        }
-        
-        public DbSet<User> Users { get; set; }
-        public DbSet<Mission> Missions { get; set; }
-        public DbSet<Question> MissionQuestions { get; set; }
-        
     }
 }
