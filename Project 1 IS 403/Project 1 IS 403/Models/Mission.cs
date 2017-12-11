653e34b38c345fbe78c538209e294771b5b200a6
hilary38 <32460688+hilary38@users.noreply.github.com>
2017-12-11 15:25:11 -0700

    mission and users models

--- Project 1 IS 403/Project 1 IS 403/Models/Mission.cs
+++ Project 1 IS 403/Project 1 IS 403/Models/Mission.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,13 +9,36 @@ namespace Project_1_IS_403.Models
 {
     public class Mission
     {
+        [Key]
+        [DisplayName("Mission ID")]
+        [Required(ErrorMessage ="A mission ID is required")]
         public int missID { get; set; }
+
+        [DisplayName("Mission Name")]
+        [Required(ErrorMessage = "A mission name is required")]
         public string missName { get; set; }
+
+        [DisplayName("Mission President")]
+        [Required(ErrorMessage = "A mission president is required")]
         public string missPresident { get; set; }
+
+        [DisplayName("Mission Address")]
+        [Required(ErrorMessage = "A mission ID is required")]
         public string missAddress { get; set; }
+
+        [DisplayName("Mission Language")]
+        [Required(ErrorMessage = "A mission language is required")]
         public string missLanguage { get; set; }
+
+        [DisplayName("Mission Climate")]
+        [Required(ErrorMessage = "A mission climate is required")]
         public string missClimate { get; set; }
+
+        [DisplayName("Dominant Religion")]
+        [Required(ErrorMessage = "A dominant religion is required")]
         public string missDominantReligion { get; set; }
+
+        [DisplayName("Mission Image")]
         public string missionImg { get; set; }
     }
 }
---
+++ Project 1 IS 403/Project 1 IS 403/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project_1_IS_403.Models
+{
+    public class User
+    {
+        public int userID { get; set; }
+
+        public string userEmail { get; set; }
+
+        public string userPassword { get; set; }
+
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+
+    }
+}
