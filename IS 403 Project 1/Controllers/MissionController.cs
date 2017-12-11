fdc49946f42a49423d0726e2eb001b55b9c9b339
mizozobu <hamc.m.so.j12@gmail.com>
2017-12-11 16:26:26 -0700

    push

--- IS 403 Project 1/Controllers/MissionController.cs
+++ IS 403 Project 1/Controllers/MissionController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IS_403_Project_1.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -29,37 +30,37 @@ namespace IS_403_Project_1.Controllers
         {
             //method to pass specific mission details to view depending on the mission. Receives a parameter of mission
 
-            if (contentType.Equals("0"))
-            {
-                oMission.missName = "Florida Jacksonville Mission";
-                oMission.missPresident = "Brother Jackson";
-                oMission.missAddress = "555 Palm Tree Lane, Jacksonville, Florida";
-                oMission.missLanguage = "Portugese";
-                oMission.missClimate = "Hot and muggy!";
-                oMission.missDominantReligion = "Southern Baptist";
-                oMission.missionImg = "~/Content/Images/fjm.jpg";
-                ViewBag.ImageUrl = "fjm.jpg";
-            }
-            else if (contentType.Equals("1"))
-            {
-                oMission.missName = "San Antonio Texas Mission";
-                oMission.missPresident = "Brother Antonio";
-                oMission.missAddress = "555 No Tree Lane, San Antonio, Texas";
-                oMission.missLanguage = "Spanish";
-                oMission.missClimate = "Hot and dry!";
-                oMission.missDominantReligion = "They're all athiests.";
-                oMission.missionImg = "~/Content/Images/san_antonio_texas_mission.png";
-            }
-            else if (contentType.Equals("2"))
-            {
-                oMission.missName = "California Oakland/ San Francisco Mission";
-                oMission.missPresident = "Brother Oakland";
-                oMission.missAddress = "555 Palm Tree Lane, San Francisco, California";
-                oMission.missLanguage = "English";
-                oMission.missClimate = "Hot and dryyyyyyyyyy!";
-                oMission.missDominantReligion = "They're all athiests.";
-                oMission.missionImg = "~/Content/Images/oakland.png";
-            }
+            //if (contentType.Equals("0"))
+            //{
+            //    oMission.missName = "Florida Jacksonville Mission";
+            //    oMission.missPresident = "Brother Jackson";
+            //    oMission.missAddress = "555 Palm Tree Lane, Jacksonville, Florida";
+            //    oMission.missLanguage = "Portugese";
+            //    oMission.missClimate = "Hot and muggy!";
+            //    oMission.missDominantReligion = "Southern Baptist";
+            //    oMission.missionImg = "~/Content/Images/fjm.jpg";
+            //    ViewBag.ImageUrl = "fjm.jpg";
+            //}
+            //else if (contentType.Equals("1"))
+            //{
+            //    oMission.missName = "San Antonio Texas Mission";
+            //    oMission.missPresident = "Brother Antonio";
+            //    oMission.missAddress = "555 No Tree Lane, San Antonio, Texas";
+            //    oMission.missLanguage = "Spanish";
+            //    oMission.missClimate = "Hot and dry!";
+            //    oMission.missDominantReligion = "They're all athiests.";
+            //    oMission.missionImg = "~/Content/Images/san_antonio_texas_mission.png";
+            //}
+            //else if (contentType.Equals("2"))
+            //{
+            //    oMission.missName = "California Oakland/ San Francisco Mission";
+            //    oMission.missPresident = "Brother Oakland";
+            //    oMission.missAddress = "555 Palm Tree Lane, San Francisco, California";
+            //    oMission.missLanguage = "English";
+            //    oMission.missClimate = "Hot and dryyyyyyyyyy!";
+            //    oMission.missDominantReligion = "They're all athiests.";
+            //    oMission.missionImg = "~/Content/Images/oakland.png";
+            //}
 
             return View(oMission);
         }
