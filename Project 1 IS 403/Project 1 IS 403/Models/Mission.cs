ce4ef0f97fa0c48b67bb1ae6271563f86d4ec8a2
hilary38 <32460688+hilary38@users.noreply.github.com>
2017-12-11 15:11:14 -0700

    mission model

--- Project 1 IS 403/Project 1 IS 403/Models/Mission.cs
+++ Project 1 IS 403/Project 1 IS 403/Models/Mission.cs
@@ -7,6 +7,7 @@ namespace Project_1_IS_403.Models
 {
     public class Mission
     {
+        public int missID { get; set; }
         public string missName { get; set; }
         public string missPresident { get; set; }
         public string missAddress { get; set; }
