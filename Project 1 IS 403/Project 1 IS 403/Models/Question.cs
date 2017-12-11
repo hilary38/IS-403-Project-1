2838cd673946ca6787d8ed51d4f69a98c5a75608
hilary38 <32460688+hilary38@users.noreply.github.com>
2017-12-11 15:32:05 -0700

    adding Question model

---
+++ Project 1 IS 403/Project 1 IS 403/Models/Question.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project_1_IS_403.Models
+{
+    public class Question
+    {
+        [Key]
+        [DisplayName("Question ID")]
+        [Required(ErrorMessage = "A question ID is required")]
+        public int questionID { get; set; }
+
+        [DisplayName("Mission ID")]
+        [Required(ErrorMessage = "A mission ID is required")]
+        public int missID { get; set; }
+
+        [DisplayName("User ID")]
+        [Required(ErrorMessage = "A user ID is required")]
+        public int userID { get; set; }
+
+        [DisplayName("Question")]
+        [Required(ErrorMessage = "A question is required")]
+        public string question { get; set; }
+
+        [DisplayName("Answer")]
+        public string answer { get; set; }
+
+    }
+}
