ee38e7447fb3051f9b0153490acfbfdbac6a4fd3
mizozobu <hamc.m.so.j12@gmail.com>
2017-12-11 16:03:30 -0700

    dal

---
+++ IS 403 Project 1/DAL/MissionContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IS_403_Project_1.DAL
+{
+    public class MissionContext : dbContext
+    {
+    }
+}
