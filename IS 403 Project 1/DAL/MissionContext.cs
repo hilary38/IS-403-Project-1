a9a2fe076cfa3fb5978a38fb5ffc8e22fdf2e28e
hilary38 <32460688+hilary38@users.noreply.github.com>
2017-12-11 16:33:58 -0700

    im scared of git

--- IS 403 Project 1/DAL/MissionContext.cs
+++ IS 403 Project 1/DAL/MissionContext.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using IS_403_Project_1.Models;
 
 namespace IS_403_Project_1.DAL
 {
-    //public class MissionContext : dbContext
-    //{
-    //}
+    public class MissionContext : DbContext
+    {
+        public MissionContext() : base("MissionContext")
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Mission> Missions { get; set; }
+        public DbSet<Question> MissionQuestions { get; set; }
+
+    }
 }
